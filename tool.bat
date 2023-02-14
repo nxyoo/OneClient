78fd80a47c444e7852fae1d8a6f26e2107eb374d
nxyoo <nxyoo@outlook.fr>
2023-02-14 11:48:14 +0100

    done i guess

--- tool.bat
+++ tool.bat
@@ -7,19 +7,6 @@ if not exist "%CD%\node-v%ver%-win-x%arch%" mkdir "%CD%\node-v%ver%-win-x%arch%"
 PATH=%PATH%;"%CD%\node-v%ver%-win-x%arch%"
 title node-js portable version %ver% LTS
 
-if not exist "%CD%\node-v%ver%-win-x%arch%\node.exe" (
-    echo installation de node-js portable version %ver% LTS
-    if not exist "%CD%\cache" mkdir "%CD%\cache"
-
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.exe' '%CD%\node-v%ver%-win-x%arch%\7z.exe'"
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://luuxis.legtux.org/res/craftdium/7z.dll' '%CD%\node-v%ver%-win-x%arch%\7z.dll'"
-    powershell "Import-Module BitsTransfer; Start-BitsTransfer 'https://nodejs.org/dist/v%ver%/node-v%ver%-win-x%arch%.zip' '%CD%\cache\node-js.zip'"
-
-    7z x -o"%CD%\" "%CD%\cache\node-js.zip"
-    del "%CD%\node-v%ver%-win-x%arch%\7z.exe"
-    del "%CD%\node-v%ver%-win-x%arch%\7z.dll"
-    rmdir "%CD%\cache" /S /Q
-)
 cls
 :launch
 echo node:   start nodejs
